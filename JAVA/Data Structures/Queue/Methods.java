1f1653b93533d7c63d388b7dc8de0a47e256452b
paramesh <parameshdarshan1234@gmail.com>
2025-01-02 23:44:04 +0530

    Notes on queue

---
+++ JAVA/Data Structures/Queue/Methods.java
@@ -0,0 +1,26 @@
+package Queue;
+
+public class Methods {
+
+    /*
+     * The methods that throw an exception for an empty queue are:
+     * E add() // It insists on inserting the element and throws an exception if it
+     * can't.
+     * E element() // retrieve but do not remove the head element
+     * E remove() // retrieve and remove the head element
+     */
+
+     /*
+      * The methods that return null for an empty queue are:
+      * boolean offer (E e) // insert the given element if possible
+      * E peek() // retrieve but do not remove the head element
+      * E poll() // retrieve and remove the head element
+      */
+
+     /*Implemented using
+      * Array
+      singly linked list
+      doubly linked list
+
+      */
+}
