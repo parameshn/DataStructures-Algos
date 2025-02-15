0389ce903dcf1af3b0d92e8e5b876d2340af1656
paramesh <parameshdarshan1234@gmail.com>
2025-02-15 22:24:50 +0530

    Lits function implementation

--- JAVA/Data Structures/List/ListComparision.java
+++ JAVA/Data Structures/List/ListComparision.java
@@ -116,5 +116,110 @@ public class ListComparision {
         System.out.println("ArrayList after add operation: " + arrayList);
 
 
+        Collections.sort(arrayList);
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        Collections.sort(arrayList, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        System.out.println("ArrayList after sorting: " + arrayList);
+        
+        arrayList.sort(Comparator.reverseOrder());
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        arrayList.sort(Comparator.naturalOrder());
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        arrayList.sort(Comparator.comparing(String::length));
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+       
+        arrayList.sort(Comparator.comparing(String::length).reversed());
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        
+        arrayList.sort(Comparator.comparing(String::length).thenComparing(Comparator.naturalOrder()));
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        arrayList.sort(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder()));
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        arrayList.sort(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder()).reversed());
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        arrayList.sort(Comparator.comparing(String::length).thenComparing(Comparator.reverseOrder()).reversed().reversed());
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        Collections.sort(arrayList, Collections.reverseOrder());
+        System.out.println("ArrayList after sorting: " + arrayList);
+
+        System.out.println("===Clone===");
+        List<String> cloneList = new ArrayList<>(arrayList);
+        System.out.println("Is clone List empty: " + cloneList.isEmpty());
+        System.out.println("Cloned List: " + cloneList);
+
+        String[] array = arrayList.toArray(new String[0]);
+        System.out.println("Array: " + Arrays.toString(array));
+        System.out.println("Array: " + Arrays.toString(arrayList.toArray()));
+        System.out.println("Array:" + array[1]);
+        array = arrayList.toArray(new String[10]);
+        System.out.println("Array: " + Arrays.toString(array));
+        array = arrayList.toArray(new String[1]);// Java creates a new array of size
+        System.out.println("Array: " + Arrays.toString(array));
+
+        arrayList.addAll(Arrays.asList("peach", "berry", "dragonfruit"));
+        System.out.println("ArrayList : " + arrayList);
+
+        arrayList.removeAll(Arrays.asList("kiwi"));
+        System.out.println("ArrayList :" + arrayList);
+
+        arrayList.retainAll(Arrays.asList("apple", "banana"));
+        System.out.println("ArrayList :" + arrayList);
+
+        arrayList.clear();
+        System.out.println("ArrayList :" + arrayList);
+
+        List<String> unModifiable = Collections.unmodifiableList(cloneList);
+        System.out.println("Unmodifiable List: " + unModifiable);
+
+        try{
+            unModifiable.add("kiwi");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+        List<String> syncList = Collections.synchronizedList(cloneList);
+        System.out.println("Synchronized List: " + syncList);
+        synchronized (syncList) {
+            for (String s : syncList) {
+                System.out.println(s);
+            }
+        }
+       
+        List<String> copyOnWriteList = new CopyOnWriteArrayList<>(cloneList);
+        System.out.println("CopyOnWrite List: " + copyOnWriteList);
+
+        copyOnWriteList.add("kiwi");
+        System.out.println("CopyOnWrite List: " + copyOnWriteList);
+
+        copyOnWriteList.sort(Comparator.naturalOrder());
+        System.out.println("CopyOnWrite List: " + copyOnWriteList);
+
+        copyOnWriteList.remove("kiwi");
+        System.out.println("CopyOnWrite List: " + copyOnWriteList);
+
+        int c = copyOnWriteList.lastIndexOf("kiwi");
+        copyOnWriteList.remove(c);
+        System.out.println("CopyOnWrite List: " + copyOnWriteList);
+
+        System.out.println(copyOnWriteList.contains("kiwi"));
+
+        try{
+            System.out.println(Integer.valueOf(copyOnWriteList.indexOf("kiwi")).equals(2));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+
+
     }
 }
